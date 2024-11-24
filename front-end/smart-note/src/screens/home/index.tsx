09f0736bb4966bf397c9b05bc50a87dc4ee5395e
spsomnath <spsomnath100@gmail.com>
2024-11-24 20:42:43 +0530

    project initialized

---
+++ front-end/smart-note/src/screens/home/index.tsx
@@ -0,0 +1,71 @@
+import {useEffect, useRef, useState} from 'react'
+
+
+export default function home() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [isDrawing, setIsDrawing] = useState(false);
+
+    useEffect( ()=> {
+        const canvas = canvasRef.current;
+
+        if (canvas){
+            const ctx = canvas.getContext('2d');
+            if (ctx){
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight - canvas.offsetTop;
+                ctx.lineCap = 'round';
+                ctx.lineWidth = 3;
+
+            }
+
+        }
+    }, []);
+
+    const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
+        const canvas = canvasRef.current;
+        if (canvas){
+            canvas.style.background ='black';
+            const ctx = canvas.getContext('2d');
+            if (ctx) {
+                ctx.beginPath();
+                ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+                setIsDrawing(true)
+
+
+            }
+        }
+    }
+
+    const stopDrawing = () =>{
+        setIsDrawing(false);
+    }
+
+    const draw = (e : React.MouseEvent<HTMLCanvasElement>) => {
+        if (! isDrawing){
+            return;
+        }
+        const canvas = canvasRef.current;
+        if (canvas){
+            const ctx = canvas.getContext('2d');
+            if(ctx) {
+                ctx.strokeStyle = 'white'
+                ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+                ctx.stroke();
+
+            }
+
+        }
+    }
+        
+    
+    return (
+        < canvas
+        ref = {canvasRef}
+        id = 'canvaas'
+        className='absolute top-0 left-0 w-full h-full'
+        onMouseDown={startDrawing}
+        onMouseOut={stopDrawing}
+        onMouseUp={startDrawing}
+        />
+    )
+}
