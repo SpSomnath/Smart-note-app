e3dbe49939f806b0207e1b35e46f60a59fcfe400
spsomnath <spsomnath100@gmail.com>
2024-12-19 23:41:39 +0530

    frontendComplete

--- front-end/smart-note/src/screens/home/index.tsx
+++ front-end/smart-note/src/screens/home/index.tsx
@@ -1,9 +1,42 @@
-import {useEffect, useRef, useState} from 'react'
+import {useEffect, useRef, useState} from 'react';
+import {SWATCHES} from '@/constant';
+import {ColorSwatch, Group } from '@mantine/core';
+import {Button} from '@/components/ui/button';
+import axios from 'axios';
+import { setDefaultResultOrder } from 'dns';
+import { Calculator } from 'lucide-react';
+
+
+interface Response {
+    expr : string;
+    result : string;
+    assign : boolean ;
+
+}
+interface GenerateResult {
+    expression : string;
+    answer : string;
+
+}
 
 
 export default function Home() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [isDrawing, setIsDrawing] = useState(false);
+    const [color, setColor] = useState('rgb(255,255,255');
+    const [reset, setReset] = useState(false);
+    const [result, setResult] = useState<GenerateResult>();
+    const [dictOfVars, setDictOfVars] = useState({});
+
+
+    useEffect(()=>{
+        if(reset){
+            resetCanvas();
+            setReset(false);
+
+        }
+    }, [reset]);
+
 
     useEffect( ()=> {
         const canvas = canvasRef.current;
@@ -20,6 +53,36 @@ export default function Home() {
 
         }
     }, []);
+
+    const sendData = async ()=>{
+        const canvas = canvasRef.current;
+
+        if (canvas) {
+            const response = await axios({
+                method:'post',
+                url : `${import.meta.env.VITE_API_URL}/calculate`,
+                data : {
+                    image : canvas.toDataURL('image/png'),
+                    dict_of_vars : dictOfVars,
+                }
+            });
+            const resp =  await response.data;
+            console.log('Response: ', resp);
+        }
+        
+
+    };
+
+    const resetCanvas = ()=>{
+        const canvas = canvasRef.current;
+        if (canvas) {
+            const ctx = canvas.getContext('2d');
+            if (ctx) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+            }
+            
+        }
+    }
 
     const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
         const canvas = canvasRef.current;
@@ -48,7 +111,7 @@ export default function Home() {
         if (canvas){
             const ctx = canvas.getContext('2d');
             if(ctx) {
-                ctx.strokeStyle = 'white'
+                ctx.strokeStyle = color
                 ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
                 ctx.stroke();
 
@@ -60,8 +123,32 @@ export default function Home() {
     
     return (
         <>
-        <h1>hello world</h1>
-        < canvas
+        <div className='grid  grid-cols-3 gap-2'>
+            <Button 
+            onClick={()=> setReset(true)}
+            className='z-20 bg-black text-white'
+            variant ='default'
+            color = 'black' >
+                Reset
+            </Button>
+            <Group className='z-20'>
+                {SWATCHES.map((swatchColor: string ) =>(
+                    <ColorSwatch
+                    key={swatchColor}
+                    color={swatchColor}
+                    onClick={()=> setColor(swatchColor)}
+                    />
+                ))}
+            </Group>
+            <Button 
+            onClick={sendData}
+            className='z-20 bg-black text-white'
+            variant ='default'
+            color = 'black' >
+                Calculate
+            </Button>
+        </div>
+                < canvas
         ref = {canvasRef}
         id = 'canvaas'
         className='absolute top-0 left-0 w-full h-full'
@@ -71,5 +158,6 @@ export default function Home() {
         onMouseUp={stopDrawing}
         />
         </>
+
     )
 }
